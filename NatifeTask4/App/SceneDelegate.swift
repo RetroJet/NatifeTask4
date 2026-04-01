4bdcb30e40fc79c39b174c7d4bff8d131b0ca914
RetroJet <rretrojet@gmail.com>
2026-04-01 11:52:30 +0200

    Initial Commit

---
+++ NatifeTask4/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  NatifeTask4
+//
+//  Created by Nazar on 01.04.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ NatifeTask4/Features/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NatifeTask4
+//
+//  Created by Nazar on 01.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
